0fe4dc86342ed7a594dd19af87db3e77d898fcb7
Emanuel Conte <emanuelcontecardoso@gmail.com>
2024-07-15 12:11:15 -0300

    Adding Authentication with JWT and Spring Security

--- first_springboot/rest-springboot-java/src/main/resources/db/migration/V8__Insert_Data_In_Users.sql
+++ first_springboot/rest-springboot-java/src/main/resources/db/migration/V8__Insert_Data_In_Users.sql
@@ -1,3 +1,3 @@
 ﻿INSERT INTO `users` (`user_name`, `full_name`, `password`, `account_non_expired`, `account_non_locked`, `credentials_non_expired`, `enabled`) VALUES
-	('emanuel', 'Emanuel Conte', '19bbf735b27066f2f145e602624e1b24a3fbc54cd5dfd3143fc5feea6bdee9e139ca7332d4806b9f', b'1', b'1', b'1', b'1'),
-	('deide', 'Deide Costa', '75ec349c1b0ef4ee7b249d0b83ae4861853f3aa77bce8c4b15f28cd43c6424ab4f29df431831bb0d', b'1', b'1', b'1', b'1');
+	('emanuel', 'Emanuel Conte', '2c6ba4b47e7bee7657e39c6c5b86c4f0894b8e5b45cc028f6897f4032534e05c7473d4675699d55d', b'1', b'1', b'1', b'1'),
+	('deide', 'Deide Costa', 'b6d5266c6e2a937bfa174ca1dedc6a6faeab25b793e07b96fa93d1b7bb2b989aa071cb2b1a924dbd', b'1', b'1', b'1', b'1');
